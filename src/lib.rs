1bda78fafd8a38dc44827e69a52fd65e4da0e2f3
Csanyi David <csanyidavid2000@gmail.com>
2023-06-02 22:09:03 +0200

    add arc and node, count

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 pub mod graph {
     pub trait Graph {
-        
+        fn node_count(&self) -> usize;
+        fn arc_count(&self) -> usize;
+        fn add_node(&mut self, v: u32);
+        fn add_arc(&mut self, a: Arc);
+
     }
 
     pub struct Arc {
@@ -8,6 +12,25 @@ pub mod graph {
         t: u32, //target
         c: i32, //cost
     }
+
+    impl Arc {
+        pub fn new(s: u32, t: u32, c: i32) -> Arc {
+            Arc{s, t, c}
+        }
+
+        pub fn source(&self) -> u32 {
+            self.s
+        }
+
+        pub fn target(&self) -> u32 {
+            self.t
+        }
+
+        pub fn cost(&self) -> i32 {
+            self.c
+        }
+    }
+
     pub struct ListDigraph {
         nodes: Vec<u32>,
         arcs: Vec<Arc>,
@@ -23,6 +46,20 @@ pub mod graph {
     }
 
     impl Graph for ListDigraph{
+        fn node_count(&self) -> usize {
+            self.nodes.len()
+        }
 
+        fn arc_count(&self) -> usize {
+            self.arcs.len()
+        }
+
+        fn add_node(&mut self, v: u32){
+            self.nodes.push(v);
+        }
+
+        fn add_arc(&mut self, a: Arc) {
+            self.arcs.push(a);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use dijkstra_in_rust::graph::ListDigraph;
+use dijkstra_in_rust::graph::{ListDigraph, Arc, Graph};
 
 fn main() {
-    let _g = ListDigraph::new();
-    println!("Hello, world!");
+    let mut g = ListDigraph::new();
+    println!("Node cnt: {}", g.node_count());
+    println!("Arc cnt: {}", g.arc_count());
+    g.add_node(0);
+    g.add_node(1);
+    g.add_arc(Arc::new(0, 1, 0));
+    println!("Node cnt: {}", g.node_count());
+    println!("Arc cnt: {}", g.arc_count());
 }
